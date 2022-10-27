9dfd9c1ce83e7d0bd791f0f2e35fdffac3715456
DezsiCsaba <111079927+DezsiCsaba@users.noreply.github.com>
2022-10-27 16:25:30 +0200

    Update Snake.py

--- My_Snake_Proj/Snake.py
+++ My_Snake_Proj/Snake.py
@@ -1,13 +1,18 @@
-#   --version 2.1
+#   --version 2.2
+#   --AI implemented
+#       -basic movement is kinda working
+#       -sometimes it gets stuck in a loop : back and forth -> need to make a rule for that by saving the last move and not letting to do the opposite of it
 
 from cmath import rect
 from operator import length_hint
 from pickle import FALSE, TRUE
 from random import randint, random
+from sre_parse import State
 from tkinter.tix import Tree
 import pygame
 import math
 from time import sleep
+
 
 pygame.init()
 
@@ -29,6 +34,7 @@ canvas = pygame.display.set_mode((width, height))
 white = (255, 255, 255)
 transp_white = (255,255,255,127)
 red = (255, 0, 0)
+transp_red = (255, 0, 0, 125)
 blue = (0, 0, 125)
 green = (0, 125, 0)
 transp_green = (0, 125, 0, 150)
@@ -74,6 +80,22 @@ def Snekk(snake_block, snake_list):
         #pygame.draw.rect(canvas,blue,(x[0], x[1], snake_block, snake_block))
         pygame.draw.rect(canvas, blue, (x[2]))
 
+
+
+def draw_rect_alpha(surface, color, rect):
+    shape_surf = pygame.Surface(pygame.Rect(rect).size, pygame.SRCALPHA)
+    pygame.draw.rect(shape_surf, color, shape_surf.get_rect())
+    surface.blit(shape_surf, rect)
+    #draws a RECtangle with a set alpha and set color
+
+def ShowVision_Area(vision_list, color):
+    for x in vision_list:
+        draw_rect_alpha(canvas, color, (x[0], x[1], snake_block, snake_block))
+#----------------SNAKE----------------
+
+
+
+#----------------BRAIN_OF_AI----------------
 def SnakeVision(snake_block, snake_list, sizeOfVision):
     HeadOfSnake = []
     sizeofSnake = len(snake_list)
@@ -88,6 +110,7 @@ def SnakeVision(snake_block, snake_list, sizeOfVision):
             pos_y = heaady - (j * snake_block)
             pos.append(pos_x)
             pos.append(pos_y)
+            pos.append(pygame.Rect(pos_x, pos_y, snake_block, snake_block))
             Vision.append(pos)
     for i in range (0, sizeOfVision):
         for j in range(0, sizeOfVision):
@@ -96,6 +119,7 @@ def SnakeVision(snake_block, snake_list, sizeOfVision):
             pos_y = heaady + (j * snake_block)
             pos.append(pos_x)
             pos.append(pos_y)
+            pos.append(pygame.Rect(pos_x, pos_y, snake_block, snake_block))
             Vision.append(pos)
     for i in range (0, sizeOfVision):
         for j in range(0, sizeOfVision):
@@ -104,6 +128,7 @@ def SnakeVision(snake_block, snake_list, sizeOfVision):
             pos_y = heaady - (j * snake_block)
             pos.append(pos_x)
             pos.append(pos_y)
+            pos.append(pygame.Rect(pos_x, pos_y, snake_block, snake_block))
             Vision.append(pos)
     for i in range (0, sizeOfVision):
         for j in range(0, sizeOfVision):
@@ -112,30 +137,132 @@ def SnakeVision(snake_block, snake_list, sizeOfVision):
             pos_y = heaady + (j * snake_block)
             pos.append(pos_x)
             pos.append(pos_y)
+            pos.append(pygame.Rect(pos_x, pos_y, snake_block, snake_block))
             Vision.append(pos)
     return Vision
+def DangerArea(snake_block, snake_list, sizeOfVision = 2):
+    HeadOfSnake = []
+    sizeofSnake = len(snake_list)
+    HeadOfSnake = snake_list[sizeofSnake - 1]
+    headX = HeadOfSnake[0]
+    heaady = HeadOfSnake[1]
+    Vision = []
+    for i in range (0, sizeOfVision):
+        for j in range(0, sizeOfVision):
+            pos = []
+            pos_x = headX + (i * snake_block)
+            pos_y = heaady - (j * snake_block)
+            pos.append(pos_x)
+            pos.append(pos_y)
+            pos.append(pygame.Rect(pos_x, pos_y, snake_block, snake_block))
+            Vision.append(pos)
+    for i in range (0, sizeOfVision):
+        for j in range(0, sizeOfVision):
+            pos = []
+            pos_x = headX - (i * snake_block)
+            pos_y = heaady + (j * snake_block)
+            pos.append(pos_x)
+            pos.append(pos_y)
+            pos.append(pygame.Rect(pos_x, pos_y, snake_block, snake_block))
+            Vision.append(pos)
+    for i in range (0, sizeOfVision):
+        for j in range(0, sizeOfVision):
+            pos = []
+            pos_x = headX - (i * snake_block)
+            pos_y = heaady - (j * snake_block)
+            pos.append(pos_x)
+            pos.append(pos_y)
+            pos.append(pygame.Rect(pos_x, pos_y, snake_block, snake_block))
+            Vision.append(pos)
+    for i in range (0, sizeOfVision):
+        for j in range(0, sizeOfVision):
+            pos = []
+            pos_x = headX + (i * snake_block)
+            pos_y = heaady + (j * snake_block)
+            pos.append(pos_x)
+            pos.append(pos_y)
+            pos.append(pygame.Rect(pos_x, pos_y, snake_block, snake_block))
+            Vision.append(pos)
+    return Vision
+def is_collision(snake_list, HeadOfSnake, walls):
+    HeadOfSnake = snake_list[len(snake_list) - 1]
+    for i in snake_list:
+            for wall in walls:
+                if pygame.Rect.colliderect(i[2], wall):
+                    return True
 
-def draw_rect_alpha(surface, color, rect):
-    shape_surf = pygame.Surface(pygame.Rect(rect).size, pygame.SRCALPHA)
-    pygame.draw.rect(shape_surf, color, shape_surf.get_rect())
-    surface.blit(shape_surf, rect)
-    #draws a RECtangle with a set alpha and set color
+    for i in snake_list[:-1]: #Checks from the end of the list
+            if i == HeadOfSnake:  # if i(part of the snake)
+               return True   # collides with the head
+    return False
+def would_it_collide(future_rect, snake_list, walls):
+    for wall in walls:
+        if pygame.Rect.colliderect(future_rect, wall): return True
+    for i in snake_list[:-1]: #Checks from the end of the list
+            if i == future_rect:  # if i(part of the snake)
+               return True   # collides with the head
+    return False
 
-def ShowVision_Area(vision_list):
-    for x in vision_list:
-        draw_rect_alpha(canvas, transp_white, (x[0], x[1], snake_block, snake_block))
-#----------------SNAKE----------------
+def get_state(snake_block, snake_list, walls, foodx, foody):
+    state = []
+    sizeofSnake = len(snake_list)
+    HeadOfSnake = snake_list[sizeofSnake - 1]
 
-#----------------AI_MOVEMENT----------------
+    headx, heady = HeadOfSnake[0], HeadOfSnake[1]    
+    left = pygame.Rect(headx - snake_block, heady, snake_block, snake_block)
+    point_right = [headx + snake_block, heady]
+    right = pygame.Rect(headx + snake_block, heady, snake_block, snake_block)
+    up = pygame.Rect(headx, heady - 20, snake_block, snake_block)
+    down = pygame.Rect(headx, heady + 20, snake_block, snake_block)
+    state.append(would_it_collide(left, snake_list, walls)) # [0] -> danger left (bool)
+    state.append(would_it_collide(right, snake_list, walls))# [1] -> danger right (bool)
+    state.append(would_it_collide(up, snake_list, walls))# [2] -> danger up (bool)
+    state.append(would_it_collide(down, snake_list, walls))# [3] -> danger down (bool)
 
-#----------------AI_MOVEMENT----------------
+    if foodx < headx : state.append('left') # [4] -> location of food (left, right)
+    elif foodx > headx : state.append('right') # [4] -> location of food (left, right)
+    else : state.append('inLine')
+    
+    if foody < heady : state.append('up') # [5] -> location of food (up, down)
+    elif foody > heady : state.append('down') # [5] -> location of food (up, down)
+    else : state.append('inLine')
+
+    return state
+#state:    
+    # [0] -> danger left (bool)
+    # [1] -> danger right (bool)
+    # [2] -> danger up (bool)
+    # [3] -> danger down (bool)
+    # [4] -> location of food (left, right)
+    # [5] -> location of food (up, down)
+
+def AI_movement_output(state):   
+    action = ''
+    if state[4] == 'left' and state[0] == False:
+        action = state[4]
+        return action 
+    elif state[4] == 'right' and state[1] == False:
+        action = 'right'
+        return action 
+    else:
+        if state[5] == 'up' and state[2] == False:
+            action = 'up'
+            return action
+        elif state[5] == 'down' and state[5] == False:
+            action  = 'down'
+            return action 
+
+#----------------BRAIN_OF_AI----------------
+
+
+
 
 #----------------APPLE----------------
 def DrawApple(x_pos, y_pos):
     pygame.draw.rect(canvas,red,(x_pos, y_pos, snake_block, snake_block))
 #----------------APPLE----------------
 
-def RunGame_withInputs():
+def Run_With_Inputs():
     game_over = False
 
     xpos = width / 2
@@ -178,12 +305,7 @@ def RunGame_withInputs():
         ypos += y_change
             #inputs
         
-            #Checks for collision between the snake and the wall barrier
-        for i in snake_list:
-            for wall in walls:
-                if pygame.Rect.colliderect(i[2], wall):
-                    game_over = TRUE
-                    GameOver()
+        
 
         snake_Head = [] #[0-xpos, 1-ypos, 2-RECT]
         snake_Head.append(xpos)
@@ -194,19 +316,27 @@ def RunGame_withInputs():
         if len(snake_list) > Length_of_Snake: #Deletes the tail of the snake
             del snake_list[0]                 # so we can make it 'move' 
         
-        for i in snake_list[:-1]: #Checks from the end of the list
-            if i == snake_Head:   # if i(part of the snake)
-               game_over = True   # collides with the head
-               GameOver()         # THEN it is GameOver
-
+        head_of_snake = snake_list[len(snake_list) - 1]
+        if is_collision(snake_list, head_of_snake, walls) == True:
+            game_over = True
+            GameOver()
         
         vision = []
+        dangerZone = []
         vision = SnakeVision(snake_block, snake_list, 5)
-        for pos in vision:
-            print(pos[0], pos[1])
-        ShowVision_Area(vision)
+        dangerZone = DangerArea(snake_block, snake_list)
+        #for pos in vision:
+            #print(pos[0], pos[1])
+        ShowVision_Area(vision, transp_white)
+        ShowVision_Area(dangerZone, transp_red)
         DrawApple(foodx, foody)
         Snekk(snake_block, snake_list)
+
+        
+        CurrentState = get_state(snake_block, snake_list, walls, foodx, foody)
+        print(CurrentState)
+
+
         pygame.display.update()
         if game_over == FALSE:
             canvas.fill(black)
@@ -221,8 +351,7 @@ def RunGame_withInputs():
         clock.tick(24)
         sleep(0.1)
 
-
-def RunGame():
+def Run_AI():
     game_over = False
 
     xpos = width / 2
@@ -246,114 +375,68 @@ def RunGame():
         walls = DrawWalls(red)
         
         
-        snake_Head = []
+
+        snake_Head = [] #[0-xpos, 1-ypos, 2-RECT]
         snake_Head.append(xpos)
-        snake_Head.append(ypos)
+        snake_Head.append(ypos)        
+        snake_Head.append(pygame.Rect(xpos, ypos, snake_block, snake_block))
         snake_list.append(snake_Head)
+
+        if len(snake_list) > Length_of_Snake: #Deletes the tail of the snake
+            del snake_list[0]                 # so we can make it 'move' 
         
-        vision = []
-        vision = SnakeVision(snake_block, snake_list, 10)
-        foodIn_distance = False
-        #----------------INPUTS----------------
-        for visionBlock in vision:
-            if visionBlock[0] == foodx and visionBlock[1] == foody: ##If the snake sees the food
-                foodIn_distance = True
-                print("food in vision")
-                if foodx == snake_Head[0]:
-                    if abs(foodx - snake_Head[0]) < 1:
-                       if foody > snake_Head[1]:
-                           y_change = snake_block
-                           x_change = 0
-                       elif foody < snake_Head[1]:
-                           y_change = -snake_block
-                           x_change = 0
-                elif foodx > snake_Head[0]:                    
-                    if abs(foodx - snake_Head[0]) < 1:
-                        if foody > snake_Head[1]:
-                            y_change = snake_block
-                            x_change = 0
-                        elif foody < snake_Head[1]:
-                            y_change = -snake_block
-                            x_change = 0
-                    else : x_change = snake_block
-                elif foodx < snake_Head[0]:                    
-                    if abs(foodx - snake_Head[0]) < 1:
-                        if foody > snake_Head[1]:
-                            y_change = snake_block
-                            x_change = 0
-                        elif foody < snake_Head[1]:
-                            x_change = 0
-                            y_change = -snake_block
-                    else: x_change = - snake_block
-                break
-            
-        if foodIn_distance == False:
-            print("Food is not in distance")
-            number = randint(1, 4)
-            if number == 1:
-                x_change = -snake_block
-                y_change = 0
-            if number == 2:
-                x_change = snake_block
-                y_change = 0
-            if number == 3:
-                x_change = 0
-                y_change = snake_block
-            if number == 4:
-                x_change = 0
-                y_change = -snake_block    
-#if event.type == pygame.KEYDOWN:
-#    if event.key == pygame.K_LEFT:
-#        x_change = -snake_block
-#        y_change = 0
-#    if event.key == pygame.K_RIGHT:
-#        x_change = +snake_block
-#        y_change = 0
-#    if event.key == pygame.K_DOWN:
-#        x_change = 0
-#        y_change = +snake_block
-#    if event.key == pygame.K_UP:
-#        x_change = 0
-#        y_change = -snake_block
-        
-        xpos += x_change
-        ypos += y_change
-        #----------------INPUTS----------------
-        
-        if xpos<=0 or xpos>=width or ypos<=0 or ypos>=height:
+        head_of_snake = snake_list[len(snake_list) - 1]
+        if is_collision(snake_list, head_of_snake, walls) == True:
             game_over = True
             GameOver()
-            
-
-        if len(snake_list) > Length_of_Snake:
-            del snake_list[0]
-
-        for i in snake_list[:-1]:
-            if i == snake_Head:
-               game_over = True
-               GameOver()
-
         
+        vision = []
+        dangerZone = []
+        vision = SnakeVision(snake_block, snake_list, 5)
+        dangerZone = DangerArea(snake_block, snake_list)
         #for pos in vision:
             #print(pos[0], pos[1])
-        ShowVision_Area(vision)
+        ShowVision_Area(vision, transp_white)
+        ShowVision_Area(dangerZone, transp_red)
         DrawApple(foodx, foody)
         Snekk(snake_block, snake_list)
+
+        
+        CurrentState = get_state(snake_block, snake_list, walls, foodx, foody)
+        print(CurrentState)
+        action = AI_movement_output(CurrentState)
+        print(str(CurrentState) + "moved: " + str(action))
+        if action == 'left':
+            x_change = -snake_block
+            y_change = 0
+        elif action == 'right':
+            x_change = +snake_block
+            y_change = 0
+        elif action == 'down':
+            x_change = 0
+            y_change = +snake_block
+        elif action == 'up':
+            x_change = 0
+            y_change = -snake_block
+
+        xpos += x_change
+        ypos += y_change
+
+
         pygame.display.update()
         if game_over == FALSE:
             canvas.fill(black)
 
-        if xpos == foodx and ypos == foody:
+        if xpos == foodx and ypos == foody: #Checks the head of the snake collides with the food
             foodx = round(randint(0, width - snake_block - 10)/snake_block) * snake_block
             foody = round(randint(0, height - snake_block - 10)/snake_block) * snake_block
-            Length_of_Snake += 1
+            Length_of_Snake += 1            # if yes we add one more snake_block
             print("+1 point")
 
         #DrawSnakeHead(xpos, ypos)
         clock.tick(24)
         sleep(0.1)
 
-
-RunGame_withInputs()
-#RunGame()
+#Run_With_Inputs()
+Run_AI()
 DontClose()
