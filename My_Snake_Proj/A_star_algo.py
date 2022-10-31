90e9cd4bad5d64570ffbd21225d9b1cc33305358
DezsiCsaba <111079927+DezsiCsaba@users.noreply.github.com>
2022-10-31 20:50:21 +0100

    Update A_star_algo.py

--- My_Snake_Proj/A_star_algo.py
+++ My_Snake_Proj/A_star_algo.py
@@ -1,14 +1,22 @@
-from collections import namedtuple
-
-import string
+from itertools import count
+import this
 import pygame
-from random import randint
-from queue import PriorityQueue
 from numpy import *
+from Spot import _SpotC
+from time import sleep
+import math
 
-width = 300
-height = 300
+clock = pygame.time.Clock()
+
+block_size = 10
+width = 1280
+height = 720
+
+cols = width // block_size
+rows = height // block_size
+
 canvas = pygame.display.set_mode((width, height))
+
 
 white = (255, 255, 255)
 transp_white = (255,255,255,127)
@@ -16,8 +24,16 @@ red = (255, 0, 0)
 transp_red = (255, 0, 0, 125)
 blue = (0, 0, 125)
 green = (0, 125, 0)
+black = (0,0,0)
 
-Point = namedtuple('Point', 'x,y')
+def heuristic(a, b):
+    dist = math.hypot(a.i-b.i, a.j-b.j)
+    return dist
+
+openSet = []
+closedSet = []
+   
+
 
 def Do_not_close():
     running = True
@@ -27,80 +43,119 @@ def Do_not_close():
                 pygame.quit()
             pygame.display.update()
 
-block_size = 25
 
-def Random_point():
-    x = round(randint(0, width - block_size)/block_size) * block_size
-    return x
-
-def _draw_list_of_rect(block_list, color):
-    for i in range (len(block_list)):
-        pygame.draw.rect(canvas, color, block_list[i][2])
-
-def _walls():
-    walls = []
-    for i in range((((width//block_size) * (height//block_size)) // 100) * 50):
-        wall = []
-        x = Random_point()
-        y = Random_point()
-        r = pygame.Rect(x, y, block_size-1, block_size-1)        
-        wall.append(x)
-        wall.append(y)
-        wall.append(r)
-        walls.append(wall)
-        
-    return walls
-
-
-def _random_block(blocks, color):
-    blockInfo = []
-    for x in blocks:   
-        block = []     
-        start_x = Random_point()
-        start_y = Random_point()
-        r = pygame.Rect(start_x, start_y, block_size-1, block_size-1)
-        if pygame.Rect.colliderect(r, x[2]) == False: break
-        
-    pygame.draw.rect(canvas, color, r)
-    blockInfo.append(start_x)
-    blockInfo.append(start_y)
-    blockInfo.append(r)
-    return blockInfo     # blockinfo: [0]->x, [1]->y, [2]->rect
-
-
-def _make_matrix(width, height):
-    columns = height // block_size
-    rows = width // block_size
-    print(str(columns) + " X " + str(rows))
+def _matrixWithSpots_():    
+    print(str(cols) + " X " + str(rows))
     maze = []
-    for j in range(columns):
+
+    counter = 0
+    for i in range(cols):
         row = []
-        for i in range(rows):
-           
-            if randint(1,5) == 1:
-                    row.append("W")
+        for j in range(rows):
+            if (i == 0 and j == 0):
+                spot = _SpotC()
+                spot._SpotInit_(i, j)
+                spot.wall, spot.start, spot.finish = False, True, False    
+            elif (i == cols-1 and j == rows-1):
+                spot = _SpotC()
+                spot._SpotInit_(i, j)
+                spot.wall, spot.finish, spot.start = False, True, False
             else:
-                row.append("0")
+                spot = _SpotC()
+                spot._SpotInit_(i, j)   
                 
+            row.append(spot)        
+            spot.ShowPoint(canvas, block_size)   
+            counter += 1
+            #print(str(counter) + ". spots's: i" + str(spot.i) +" - j"+ str(spot.j) +" "+ str(spot.start)+" "+str(spot.finish)) 
         maze.append(row)
+#    for i in range(cols):
+#        for j in range(rows):
+#            spot.AddNeighbours(spot, maze, cols, rows)
     return maze
 
-def _draw_walls(maze):
-    x = 0
-    y= 0
-    for i in range(height//block_size):
-        for j in range(width//block_size):
-            if maze[i][j] == 'W':
-                pygame.draw.rect(canvas, white, (x, y, block_size-1, block_size-1))
-            if maze[i][j] == '0':
-                pygame.draw.rect(canvas, green, (x, y, block_size-1, block_size-1))
-            x += block_size
-            print("j: " + str(j))
-        y += block_size
-        print("j: " + str(i))
+
+maze = _matrixWithSpots_()
+#print(maze[0][0].i, maze[0][0].j)
+#print(maze[cols-1][rows-1].i, maze[cols-1][rows-1].j)
+#for i in range(cols):
+#    for j in range(rows):
+#        print( "-> spots's: i" + str(maze[i][j].i) +" - j"+ str(maze[i][j].j) +" "+ str(maze[i][j].start)+" "+str(maze[i][j].finish)) 
+       
+
+       
+openSet.append(maze[0][0])
+end = _SpotC()
+end._SpotInit_(cols-1, rows-1)
+end.wall, end.finish = False, True
 
 
-maze = _make_matrix(width, height)
-_draw_walls(maze)
-#print(maze)
+for i in range(cols-1):
+    for j in range(rows-1):
+        #print(i,j)
+        maze[i][j].AddNeighbours(maze, cols, rows)
+
+
+while(len(openSet) > 0):
+    #clock.tick(30)
+    #best next option
+    winner = 0
+    for i in range(len(openSet)):
+        if (openSet[i].f < openSet.__getitem__(winner).f):
+            winner = i     
+    current = openSet.__getitem__(winner)
+
+    #did we reach the finish?
+    if (current == end):
+        print("FinishedTask")
+        break
+
+    #we move the best option to the openSet
+    openSet.remove(current)
+    closedSet.append(current)
+
+    #check the neighbours
+    thisNeighbours = current.neighbours
+    for i in range(len(thisNeighbours)):
+        neighbour = _SpotC()
+        neighbour = thisNeighbours[i]
+
+        #is the next spot valid or not?
+        if (neighbour not in closedSet and neighbour.wall == False):
+            tempG = current.g + heuristic(neighbour, current)
+
+            #is the new found solution a better path?
+            newPath = False
+            if(openSet.__contains__(neighbour)):
+                if(tempG < neighbour.g):
+                    neighbour.g = tempG
+                    newPath = True
+            else:
+                neighbour.g = tempG
+                newPath = True
+                openSet.append(neighbour)
+
+            if(newPath):
+                neighbour.heuristic = heuristic(neighbour, end)
+                neighbour.f = neighbour.g + neighbour.heuristic
+                neighbour.previous = current
+    
+    if len(openSet)<1:
+        print("No solution Possible")
+
+    
+    for i in range(cols):
+        for j in range(rows):
+            maze[i][j].ShowPoint(canvas, block_size)
+
+
+    for j in range(len(openSet)):
+        openSet[i].Show(canvas, block_size, green)
+        
+    for i in range(len(closedSet)):
+        closedSet[i].Show(canvas, block_size, red)
+        print(len(closedSet))
+    pygame.display.update()
+    
+        
 Do_not_close()
