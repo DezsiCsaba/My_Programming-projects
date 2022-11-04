c85dbcdb18f066b1cd86100df49bb92cbe4ae32a
DezsiCsaba <111079927+DezsiCsaba@users.noreply.github.com>
2022-11-04 14:43:08 +0100

    Update Tile.py

--- WaveFunc_Collapse/Tile.py
+++ WaveFunc_Collapse/Tile.py
@@ -3,28 +3,28 @@ pygame.init()
 from os import listdir
 
 
-
-path = "Coding_Train_Circuit/"
-
 IMG_x = 0
 IMG_y = 0
 IMG_pos = (0,0)
 
-def _import_all_images_(dir): #given a directory, lists all the files within
-    tileImages = []           # and appends all the files to a list
-    imgs = listdir(dir)
-    for img_dir in imgs:
-        tileImages.append(pygame.image.load(str(path) + str(img_dir)))
-    return tileImages
 
 def _import_img(img_path):
     img = pygame.image.load(str(img_path))
     return img
-
-
 class Tile():
-    def _init(self):
+    image = pygame.image
+    up, right, down, left = [],[],[],[] 
+    edges = str
+    index = int 
+    w, h = int
+
+    def _init (self, img, edges, i, DIM_of_img):
+        self.image = img
+        self.edges = edges
+        self.up, self.right, self.down, self.left = [],[],[],[]
+
+        self.w, self.h = DIM_of_img
+        if (i != None): self.index = i
+
+    def rotate(self, num):
         pass
-
-
-    
