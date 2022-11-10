d1baa1c4d1904f405942ff7c78120330b6fec8ba
DezsiCsaba <111079927+DezsiCsaba@users.noreply.github.com>
2022-11-10 16:04:53 +0100

    Update WFC.py

--- WaveFunc_Collapse/WFC.py
+++ WaveFunc_Collapse/WFC.py
@@ -16,20 +16,41 @@ def DontClose():
                 pygame.quit()
             pygame.display.update()
 
-h = 400
+h = 800
 w = h
 canvas = pygame.display.set_mode((h,w))
-DIM_of_canvas = 2  # 10 -> 10 x 10 tiles
+DIM_of_canvas = 10  # 10 -> 10 x 10 tiles
 DIM_of_img = int(h/DIM_of_canvas)
 SIZE_of_IMG = (DIM_of_img, DIM_of_img)
+
+
+def ReturnOnlyDuplicates(test):
+    returnArr = np.full((test.size),None)
+    size = 0
+    for i in range(test.size):
+        for j in range(test.size)[::-1]:
+            if (i == j):
+                break
+            elif (test[i] == test[j]):
+                returnArr[size] = test[i]
+                size += 1
+    returnArr = returnArr[0:size]
+    
+    return np.unique(returnArr)
+def ShowOnScreen(x,y, input):
+    ourFont = pygame.font.Font("freesansbold.ttf", 10)
+    if (isinstance(input,np.ndarray) or isinstance(input, list)):
+        text = ourFont.render((str(input)), True, (255,0,0))
+    else : text = ourFont.render(str(input), True, (0,0,255))
+    canvas.blit(text, (x * DIM_of_img, y * DIM_of_img))
 
 
 def _setup():
     #tileimages = np.empty[2, pygame.image]
     
     tiles = np.empty(5, Tile_C)
-    img1 = pygame.image.load("Demo_Tiles/blank.png")
-    img2 = pygame.image.load("Demo_Tiles/up.png")
+    img1 = pygame.image.load("WaveFunc_Collapse\Demo_Tiles/blank.png")
+    img2 = pygame.image.load("WaveFunc_Collapse\Demo_Tiles/up.png")
     
     tile0 = Tile_C()
     tile1 = Tile_C()
@@ -186,46 +207,27 @@ def WFC():
 
 
 
-
-
-
-
-
 def WFC_withInput():
     grid = outerGrid.copy()
     imgDim = DIM_of_img
     
     #grid[3].collapsed = True
-    #grid[3].options = [tiles[2]]
+    #grid[3].options = [2]
+    #grid[3].collapsed = True
     #grid[1].collapsed = True
-    #grid[1].options = [tiles[2], tiles[3]]
+    #grid[0].options = [4]
 
     #for i in range(len(grid) + 1):
     while True:
-        for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
+        #for event in pygame.event.get():
+            #if event.type == pygame.KEYDOWN:
+                #if event.key == pygame.K_LEFT:
                     
                     print("------------------------------------------")
                     print("0.: " + str(grid[0].options) + str(grid[0].collapsed) + " \t " +"1.: " + str(grid[1].options)+ str(grid[1].collapsed))
                     print("2.: " + str(grid[2].options) + str(grid[2].collapsed) + " \t " +"3.: " + str(grid[3].options)+ str(grid[3].collapsed))
 
-                    x,y = 0,0
-                    for i in range(DIM_of_canvas):
-                        x=0
-                        for j in range(DIM_of_canvas):
-                            cell = grid[ j + i * DIM_of_canvas]
-                            if (cell.collapsed == True):
-                                index = cell.options[0]
-                                tiles[index]._ShowTile(canvas, x*imgDim, y*imgDim)
-                                #index._ShowTile(canvas, x*imgDim, y*imgDim)
-                            else:
-                                pygame.draw.rect(canvas, (0,50,0), (j*imgDim, i*imgDim, imgDim-1, imgDim-1))
-                            pygame.display.update()
-                            if(DIM_of_canvas < 4): sleep(0.1)
-                            elif(DIM_of_canvas >= 4 and DIM_of_canvas < 15): sleep(0.005)          
-                            x += 1
-                        y += 1
+                    
 
                     #Pick the cell with the lowest entropy(the number of possible options)
                     gridlist = []
@@ -253,10 +255,31 @@ def WFC_withInput():
 
                     cell.options = [pick]
 
+                    x,y = 0,0
+                    for i in range(DIM_of_canvas):
+                        x=0
+                        for j in range(DIM_of_canvas):
+                            cell = grid[ j + i * DIM_of_canvas]
+                            if (cell.collapsed == True):
+                                index = cell.options[0]
+                                tiles[index]._ShowTile(canvas, x*imgDim, y*imgDim)
+                                #index._ShowTile(canvas, x*imgDim, y*imgDim)
+                            else:
+                                pygame.draw.rect(canvas, (0,50,0), (j*imgDim, i*imgDim, imgDim-1, imgDim-1))
+                            pygame.display.update()
+                            if(DIM_of_canvas < 4): sleep(0.1)
+                            elif(DIM_of_canvas >= 4 and DIM_of_canvas < 15): sleep(0.005)          
+                            x += 1
+                        y += 1
+
+
+
                     nextGrid = np.empty(DIM_of_canvas*DIM_of_canvas, Cell_C)
                     for j in range(DIM_of_canvas):
                         for i in range(DIM_of_canvas):
-                            index = i + j * DIM_of_canvas         
+                            index = i + j * DIM_of_canvas      
+                            iscollapsed = False   
+                            collapsed = 0
                                                
                             if (grid[index].collapsed):
                                 nextGrid[index] = grid[index]
@@ -264,47 +287,78 @@ def WFC_withInput():
                                 options = np.full((len(tiles)), i)
                                 for k in range(len(tiles)):
                                     options[k] = k
+                                validArray = np.full(0,int)
                                 print(options)
-                                #---look up
+                                #---look up                                
                                 if(j > 0):
                                     up = grid[i + (j-1) * DIM_of_canvas]
-                                    validOptions = np.full((len(tiles)), None)
-                                    for option in up.options:
-                                        valid = tiles[option].down
-                                        validOptions = np.concatenate((validOptions, valid))
-                                    _checkValid(options, validOptions)
+                                    if up.collapsed == True : 
+                                        collapsed += 1
+                                        iscollapsed = True
+                                        validOptions = np.full((len(tiles)), None)
+                                        for option in up.options:
+                                            valid = tiles[option].down
+                                            validArray = np.concatenate((validArray, valid))
+                                            validOptions = np.concatenate((validOptions, valid))
+                                        _checkValid(options, validOptions)
+                                        print("lookUp," + str(validArray))
                                     
                                 #---look right
                                 if(i < DIM_of_canvas -1):
                                     right = grid[i + 1 + j * DIM_of_canvas]
-                                    validOptions = np.full((len(tiles)), None)
-                                    for option in right.options:
-                                        valid = tiles[option].left
-                                        validOptions = np.concatenate((validOptions, valid))
-                                    _checkValid(options, validOptions)
+                                    if right.collapsed == True : 
+                                        collapsed += 1
+                                        iscollapsed = True
+                                        validOptions = np.full((len(tiles)), None)
+                                        for option in right.options:
+                                            valid = tiles[option].left
+                                            validArray = np.concatenate((validArray, valid))
+                                            validOptions = np.concatenate((validOptions, valid))
+                                        _checkValid(options, validOptions)
+                                        print("lookRight," + str(validArray))
                                     
                                 #---look down
                                 if(j < DIM_of_canvas-1):
                                     down = grid[i + (j+1) * DIM_of_canvas]
-                                    validOptions = np.full((len(tiles)), None)
-                                    for option in down.options:
-                                        valid = tiles[option].up
-                                        validOptions = np.concatenate((validOptions, valid))
-                                    _checkValid(options, validOptions)
+                                    if down.collapsed == True : 
+                                        collapsed += 1
+                                        iscollapsed = True
+                                        validOptions = np.full((len(tiles)), None)
+                                        for option in down.options:
+                                            valid = tiles[option].up
+                                            validArray = np.concatenate((validArray, valid))
+                                            validOptions = np.concatenate((validOptions, valid))
+                                        _checkValid(options, validOptions)
+                                        print("lookDown," + str(validArray))
                                     
                                 #---look left
                                 if(i > 0):
                                     left = grid[i - 1 + j * DIM_of_canvas]
-                                    validOptions = np.full((len(tiles)), None)
-                                    for option in left.options:
-                                        valid = tiles[option].right
-                                        validOptions = np.concatenate((validOptions, valid))
-                                    _checkValid(options, validOptions)
+                                    if left.collapsed == True : 
+                                        collapsed += 1
+                                        iscollapsed = True
+                                        validOptions = np.full((len(tiles)), None)
+                                        for option in left.options:
+                                            valid = tiles[option].right
+                                            validArray = np.concatenate((validArray, valid))
+                                            validOptions = np.concatenate((validOptions, valid))
+                                        _checkValid(options, validOptions)
+                                        print("lookleft," + str(validArray))
                                     
 
-
+                                
                                 nextGrid[index] = Cell_C()
-                                nextGrid[index]._CellInit(options)
+                                if iscollapsed == False:
+                                    returnable = np.full((len(tiles)), i)
+                                    for k in range(len(tiles)):
+                                        returnable[k] = k
+                                elif (collapsed > 1):
+                                    returnable = ReturnOnlyDuplicates(validArray)
+                                else: returnable = valid
+
+
+                                #ShowOnScreen(i,j,returnable)
+                                nextGrid[index]._CellInit(returnable)
                                 #print(options)
                     
                     grid = nextGrid.copy()                    
@@ -328,12 +382,20 @@ pygame.display.update()
 
 WFC_withInput()
 
+test = np.full(9, int)
+test[0]=1
+test[1]=2
+test[2]=4
+test[3]=2
+test[4]=5
+test[5]=4
+test[6]=1
+test[7]=2
+test[8]=3
+print(test)
 
 
-
-
-
-
-
+test2 = ReturnOnlyDuplicates(test)
+print(test2)
 
 DontClose()
